c3ffa2378d8561e70ea958174466c508948a1fd3
JhonatanDCR <67338472+JhonatanDCR@users.noreply.github.com>
2020-12-03 18:41:40 -0500

    Add files via upload

---
+++ tienda_virtual/Models/InfoEmpresarialModel.php
@@ -0,0 +1,10 @@
+<?php
+
+    class InfoEmpresarialModel extends Mysql
+    {
+        public function __construct()
+        {
+           parent::__construct();//cargar el metodo contru de mysql
+        }
+    }
+?>
